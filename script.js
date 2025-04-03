bf1451fc34a390c6f7aeab19fc04a428b35cb163
JohnFerrancol <88333166+JohnFerrancol@users.noreply.github.com>
2025-04-03 16:07:59 +0800

    Refactored renderLibrary function

--- script.js
+++ script.js
@@ -11,12 +11,17 @@ const myLibrary = [
   new Book(crypto.randomUUID(), "Gone Girl", "Gilian Flynn", 480, true),
   new Book(crypto.randomUUID(), "Misery", "Stephen King", 300, false),
 ];
+
 const booksContainer = document.querySelector(".books-container");
 const addBookButton = document.querySelector(".add-book-button");
 const addBookDialog = document.querySelector(".add-book-dialog");
 const closeDialogIcon = document.querySelector(".close-dialog-icon");
 const submitDialogButton = document.querySelector(".submit-dialog-button");
 const addBookForm = document.querySelector("#add-book-form");
+
+window.addEventListener("load", () => {
+  renderLibrary();
+});
 
 addBookButton.addEventListener("click", () => {
   addBookDialog.showModal();
@@ -29,13 +34,13 @@ closeDialogIcon.addEventListener("click", () => {
 submitDialogButton.addEventListener("click", (event) => {
   // Prevent page refresh and sending items to the server
   event.preventDefault();
+
+  // Add the book to the myLibrary array of Book objects
   addBookToLibrary();
+
+  // Clear the form and close the dialog
   addBookForm.reset();
   addBookDialog.close();
-});
-
-window.addEventListener("load", () => {
-  renderLibrary();
 });
 
 // Defining a function which defines the book object
@@ -80,20 +85,47 @@ function addBookToLibrary() {
 function renderLibrary() {
   // If the user is adding a book or changing a read status, remove all books from the container
   booksContainer.innerHTML = "";
+
   // Iterate over the loop and addd an instance of a book element to the book container
   for (const book of myLibrary) {
-    booksContainer.innerHTML += `<div class="book">
-          <h3 class="book-title">${book.title}</h3>
-          <p class="book-author">${book.author}</p>
-          <p class="book-pages">${book.pages} pages</p>
-          <div class="button-container">
-            <button class="have-read-button">Have Read</button>
-            <img
-              src="assets/logos/delete.svg"
-              alt="Delete Book"
-              class="remove-book"
-            />
-          </div>
-        </div>`;
+    // Create book element which contains the book contents
+    const bookElement = document.createElement("div");
+    bookElement.classList.add("book");
+    bookElement.dataset.id = book.id;
+
+    // Create a book title, author and pages elements
+    const bookTitle = document.createElement("h3");
+    bookTitle.classList.add("book-title");
+    bookTitle.textContent = book.title;
+    bookElement.appendChild(bookTitle);
+
+    const bookAuthor = document.createElement("p");
+    bookAuthor.classList.add("book-author");
+    bookAuthor.textContent = book.author;
+    bookElement.appendChild(bookAuthor);
+
+    const bookPages = document.createElement("p");
+    bookPages.classList.add("book-pages");
+    bookPages.textContent = `${book.pages} pages`;
+    bookElement.appendChild(bookPages);
+
+    // Create a button container which allows users to change the read status and remove the books
+    const bookButtonContainer = document.createElement("div");
+    bookButtonContainer.classList.add("button-container");
+
+    const haveReadButton = document.createElement("button");
+    haveReadButton.textContent = "Read Book";
+    haveReadButton.classList.add("have-read-button");
+
+    const removeBookIcon = document.createElement("img");
+    removeBookIcon.src = "assets/logos/delete.svg";
+    removeBookIcon.alt = "Delete Book";
+    removeBookIcon.classList.add("remove-book-icon");
+
+    bookButtonContainer.appendChild(haveReadButton);
+    bookButtonContainer.appendChild(removeBookIcon);
+    bookElement.appendChild(bookButtonContainer);
+
+    booksContainer.appendChild(bookElement);
   }
 }
